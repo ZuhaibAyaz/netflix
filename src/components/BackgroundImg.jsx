0654f14ddea9c35279e19d69709e1ac83e67f0ca
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-08-20 19:30:23 +0530

    feat

---
+++ src/components/BackgroundImg.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BackgroundImg() {
+  return (
+    <div>BackgroundImg</div>
+  )
+}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import BackgroundImg from '../components/BackgroundImg';
+import Header from '../components/Header';
 
-function Signup() {
+export default function Signup() {
   return (
-    <div>Signup</div>
+    <Container>
+
+<BackgroundImg/>
+<Header/>
+  
+  <div className="body flex j-center a-center column">
+
+<div className="text flex column">
+
+<h1>Unlimited Movies and TV Shows and more</h1>
+<h4>Watch Anywhere. Cancle Anytime</h4>
+<h6>Ready to watch? Enter your email to create or restart your membership</h6>
+</div>
+
+
+<div className='form'>
+  <input type='email' placeholder='Email Address' name='email'/>
+  <input type='password' placeholder='Password' name='password'/>
+  <button>Get Started</button>
+
+
+</div>
+
+<button>Log In</button>
+  </div>
+
+
+    </Container>
   )
 }
 
-export default Signup
+const Container= styled.div``;
+
+
+
