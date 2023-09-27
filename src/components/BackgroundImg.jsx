6075f57b4989dcabda8c067614569520157b1c47
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-09-27 19:35:59 +0530

    feat

--- src/components/BackgroundImg.jsx
+++ src/components/BackgroundImg.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import background from '../assets/login.jpg'
 export default function BackgroundImg() {
   return (
-    <div>BackgroundImg</div>
+    <Container>
+        <img  src={background} alt='background'/>
+    </Container>
   )
+
 }
+
+const Container = styled.div`
+ height: 100vh;
+ width: 100vw;
+
+ img{
+    height: 100vh;
+    width: 100vw;
+ }
+
+`;
